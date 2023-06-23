0715bc4ad31081084bf5c46ce4de824ac75e25af
Rob Johnson <rob.johnson87@ntlworld.com>
2023-06-23 16:02:16 +0100

    reorder to match random numbers across countries

--- code/p2RandCountry.m
+++ code/p2RandCountry.m
@@ -1,10 +1,13 @@
 function data = p2RandCountry(data,CD,income_level,dis_ref,R0betafun)
 
-
+lx = data.lx;
 
 remote_quantile = unifrnd(0,1);
 labsh_quantile = unifrnd(0,1);
 bmi_quantile = unifrnd(0,1);
+international_tourism_quant = unifrnd(0,1);
+remaining_tourism_quant = unifrnd(0,1);
+Hmax_quantile = unifrnd(0,1);
 
 data.remote_quantile = remote_quantile;
 
@@ -14,25 +17,25 @@ if strcmp(income_level,'LLMIC')
     internet_coverage = betainv(remote_quantile,1.78,3.11);
     labsh = betainv(labsh_quantile,5.09,4.52);
     bmi = norminv(bmi_quantile,24.29,2.28);
-    Hmax = gamrnd(1.296265366, 1/0.049499412 );
+    Hmax = gaminv(Hmax_quantile, 1.296265366, 1/0.049499412 );
 elseif strcmp(income_level,'MIC')
     country_indices = strcmp(CD.igroup,'UMIC') | strcmp(CD.igroup,'LMIC');
     internet_coverage = betainv(remote_quantile,3.77,2.91);    
     labsh = betainv(labsh_quantile,6.29,6.54);
     bmi = norminv(bmi_quantile,25.96,2.22);
-    Hmax = gamrnd(1.360805416, 1/0.027592345);
+    Hmax = gaminv(Hmax_quantile, 1.360805416, 1/0.027592345);
 elseif strcmp(income_level,'HIC')
     country_indices = strcmp(CD.igroup,'HIC');
     internet_coverage = betainv(remote_quantile,9.57,1.39);
     labsh = betainv(labsh_quantile,7.97,6.87);
     bmi = norminv(bmi_quantile,26.81,1.52);
-    Hmax = gamrnd(2.045582620,   1/0.021471378);
+    Hmax = gaminv(Hmax_quantile, 2.045582620,   1/0.021471378);
 else
     country_indices = strcmp(CD.igroup,'LIC') | strcmp(CD.igroup,'LMIC') |...
         strcmp(CD.igroup,'UMIC') | strcmp(CD.igroup,'HIC');
     internet_coverage = 0;
     labsh = 0;
-    Hmax = lognrnd(2.83235908, 0.87983858);
+    Hmax = logninv(Hmax_quantile, 2.83235908, 0.87983858);
 end
 
 % bmi
@@ -129,67 +132,8 @@ randvalue  = table2array(CD(randindex,330));
 defivalue  = randvalue;
 data.workp = unifrnd(minwp,maxwp,1,1);
 
-% change sizes of tourism and agricultural sectors
-% rescale sectors
-% get populations
-agInd = 1;
-FAAind = 32;
-agPop = data.NNs(agInd);
-FAAPop = data.NNs(FAAind);
-otherindices = setdiff([1:45,48],[agInd,FAAind]);
 
-workingagepop = sum(data.NNs([1:45,48]));
-leftoverpop = workingagepop - agPop - FAAPop;
-
-popindices = find(~isnan(CD.NNs1)&~isnan(CD.Npop1) & country_indices);
-workers = table2array(CD(popindices,25:69));%number of workers by sector in real country
-adults = sum(table2array(CD(popindices,8:16)),2);
-
-minFAAworkers = min(workers(:,FAAind) ./ adults);
-minagWorkers = min(workers(:,agInd) ./ adults);
-maxFAAworkers = max(workers(:,FAAind) ./ sum(workers,2));
-maxagWorkers = max(workers(:,agInd) ./ sum(workers,2));
-
-FAAfrac = unifrnd( minFAAworkers , maxFAAworkers );
-agFrac = unifrnd( minagWorkers, min(maxagWorkers, 1-FAAfrac) );
-leftover_frac = 1 - FAAfrac - agFrac;
-
-% data.NNs(agInd) = agFrac * workingagepop;
-% data.NNs(FAAind) = FAAfrac * workingagepop;
-% data.NNs(otherindices) = data.NNs(otherindices)./leftoverpop .* workingagepop .* leftover_frac;
-
-%obj: income per worker
-nonempind                   = find(~isnan(CD.obj1)&~isnan(CD.NNs1) & country_indices);
-randindex                   = nonempind(randi(numel(nonempind)));
-randvalue                   = table2array(CD(randindex,331:375));%gva by sector in real country
-defivalue                   = randvalue./table2array(CD(randindex,25:69));%gva per worker by sector in real country
-defivalue(isnan(defivalue)) = 0;
-defivalue(isinf(defivalue)) = 0;
-defivalue                   = data.NNs(1:45).*defivalue';%gva by sector in artificial country
-%%!! need to check food and accomm gva as % of gdp
-data.obj                    = defivalue;
-
-
-pointiness = 5.93; % for beta distribution
-sec_to_international = 3.66; % scales fraction to fraction
-international_const = 0.0959; % constant
-
-remaining_international_tourism = lognrnd(-1.39,0.39);
-GDP = sum(data.obj);
-FAAfrac = data.obj(FAAind)/GDP;
-alpha = pointiness * min(sec_to_international * FAAfrac + international_const,1);
-beta = pointiness - alpha;
-frac_tourism_international = betarnd(alpha,beta);
-FAAmax = 1 - frac_tourism_international + frac_tourism_international*remaining_international_tourism;
-data.frac_tourism_international = frac_tourism_international;
-
-data.unmit = ones(size(data.x_elim));
-%%!! set min to one for now until we can allow for increased tourism
-data.unmit(FAAind) = min(FAAmax,1);
-data.x_elim(FAAind) = min(FAAmax,data.x_elim(FAAind));
-data.x_econ(FAAind,:) = min(FAAmax,data.x_econ(FAAind,:));
-data.x_schc(FAAind,:) = min(FAAmax,data.x_schc(FAAind,:));
-
+%%
 %wfh
 nonempind = find(~isnan(CD.wfhl1) & country_indices);
 randindex = nonempind(randi(numel(nonempind)));
@@ -313,6 +257,9 @@ discount_rate = 0.03;
 rate_of_return_one_year = 0.08;
 PV = (1-(1+discount_rate)^(-working_years))/discount_rate;
 
+adultindices = [1:lx,lx+data.adInd];
+workingagepop = sum(data.NNs(adultindices));
+
 students_affected = 1 - 0.86 * internet_coverage;
 mean_annual_income = labsh*gdp/workingagepop;
 data.labsh = labsh;
@@ -328,11 +275,96 @@ defivalue = 0.5454*gdp/sum(data.Npop(2:4));
 data.vsy  = educationloss_per_student;
 data.educationloss_all_students  = educationloss_all_students;
 
+
+
+%% change sizes of sectors
+% rescale sectors
+% get populations
+% agInd = 1;
+FAAind = 32;
+pointiness = 1000;
+
+adult_props = data.NNs(adultindices)./workingagepop;
+small_sectors = find(adult_props<1e-3);
+resample_sectors = setdiff(adultindices, adultindices(small_sectors));
+
+workingagepop2 = sum(data.NNs(resample_sectors));
+adult_props2 = data.NNs(resample_sectors)./workingagepop2;
+
+newvals = gamrnd(adult_props2*pointiness,1);
+vals = newvals ./ sum(newvals);
+% min(vals)
+% max(abs(adult_props2 - vals))
+% scatter(adult_props2,vals)
+
+data.NNs(resample_sectors) = workingagepop2 .* vals;
+
+
+% agPop = data.NNs(agInd);
+% FAAPop = data.NNs(FAAind);
+% otherindices = setdiff(adultindices,[agInd,FAAind]);
+% 
+% leftoverpop = workingagepop - agPop - FAAPop;
+
+% popindices = find(~isnan(CD.NNs1)&~isnan(CD.Npop1) & country_indices);
+% workers = table2array(CD(popindices,25:69));%number of workers by sector in real country
+% adults = sum(table2array(CD(popindices,8:16)),2);
+
+% minFAAworkers = min(workers(:,FAAind) ./ adults);
+% minagWorkers = min(workers(:,agInd) ./ adults);
+% maxFAAworkers = max(workers(:,FAAind) ./ sum(workers,2));
+% maxagWorkers = max(workers(:,agInd) ./ sum(workers,2));
+% 
+% FAAfrac = unifrnd( minFAAworkers , maxFAAworkers );
+% agFrac = unifrnd( minagWorkers, min(maxagWorkers, 1-FAAfrac) );
+% leftover_frac = 1 - FAAfrac - agFrac;
+
+% data.NNs(agInd) = agFrac * workingagepop;
+% data.NNs(FAAind) = FAAfrac * workingagepop;
+% data.NNs(otherindices) = data.NNs(otherindices)./leftoverpop .* workingagepop .* leftover_frac;
+
+%obj: income per worker
+nonempind                   = find(~isnan(CD.obj1)&~isnan(CD.NNs1) & country_indices);
+randindex                   = nonempind(randi(numel(nonempind)));
+randvalue                   = table2array(CD(randindex,331:375));%gva by sector in real country
+defivalue                   = randvalue./table2array(CD(randindex,25:69));%gva per worker by sector in real country
+defivalue(isnan(defivalue)) = 0;
+defivalue(isinf(defivalue)) = 0;
+defivalue                   = data.NNs(1:45).*defivalue';%gva by sector in artificial country
+%%!! need to check food and accomm gva as % of gdp
+data.obj                    = defivalue;
+
+%% international tourism 
+
+pointiness = 5.93; % for beta distribution
+sec_to_international = 3.66; % scales fraction to fraction
+international_const = 0.0959; % constant
+
+remaining_international_tourism = logninv(remaining_tourism_quant,-1.39,0.39);
+GDP = sum(data.obj);
+FAAfrac = data.obj(FAAind)/GDP;
+alpha = pointiness * min(sec_to_international * FAAfrac + international_const,1);
+beta = pointiness - alpha;
+frac_tourism_international = betainv(international_tourism_quant,alpha,beta);
+FAAmax = 1 - frac_tourism_international + frac_tourism_international*remaining_international_tourism;
+data.frac_tourism_international = frac_tourism_international;
+
+data.unmit = ones(size(data.x_elim));
+%%!! set min to one for now until we can allow for increased tourism
+data.unmit(FAAind) = min(FAAmax,1);
+data.x_elim(FAAind) = min(FAAmax,data.x_elim(FAAind));
+data.x_econ(FAAind,:) = min(FAAmax,data.x_econ(FAAind,:));
+data.x_schc(FAAind,:) = min(FAAmax,data.x_schc(FAAind,:));
+
+
+
+%% population disease parameters
+
 %Contact Matrix
 [basic_contact_matrix,data] = p2MakeDs(data,data.NNs,ones(data.lx,1),zeros(1,data.lx));
 data.basic_contact_matrix = basic_contact_matrix;
 
-%% get covid wt doubling time
+% get covid wt doubling time
 
 dis = population_disease_parameters(data,dis_ref,R0betafun);
 
