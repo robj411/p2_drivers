6383776eb6c9f150c7f3814c7e9645fbc28d98c2
Rob Johnson <rob.johnson87@ntlworld.com>
2023-10-19 09:11:45 +0100

    start at 0; V compartments

--- code/data_start.m
+++ code/data_start.m
@@ -6,23 +6,34 @@ function data = data_start()
     data      = rmfield(data,fields(~ismember(1:numel(fields),ikeep)));  
     data.adInd = 3;
     data.lx = length(data.B);
-    data.tvec = [-75 365*2];
+    data.tvec = [0 365*2];
     data.alp = 1;
     data.EdInd    = 41;%education sector index
     data.HospInd  = [32,43,44];%hospitality sector indices
     
+    contacts = struct;
+    contacts.B = data.B;
+    contacts.C = data.C;
+    data = rmfield(data,'B');
+    data = rmfield(data,'C');
+    data = rmfield(data,'hospA2');
+    data = rmfield(data,'hospA3');
+    data = rmfield(data,'hospA4');
+    data.contacts = contacts;
+    
     compindex = struct;
-    compindex.S_index = [1,10,11,12,13]; % S, Sn, Sp, Sv, Sb
-    compindex.E_index = [2,14,15];
-    compindex.I_index = [3:6,16:19,20:23];
-%     Ina=    y_mat(:,compindex.I_index(1));
-%     Isa=    y_mat(:,compindex.I_index(2));
-%     Ins=    y_mat(:,compindex.I_index(3));
-%     Iss=    y_mat(:,compindex.I_index(4));
-    compindex.H_index = [7,24:25];
-    compindex.R_index = [8,26:27];
-    compindex.D_index = [9];
-    compindex.V_index = [28];
+    
+    %%!! need to add Spb - transition to Sb
+    compindex.S_index = [1, 8:11]; % S, Sn, Sp, Sv, Sb
+    compindex.E_index = [2, 12:13];
+    compindex.I_index = [3:4, 14:15,16:17];
+%     Ia=    y_mat(:,compindex.I_index(1));
+%     Is=    y_mat(:,compindex.I_index(2));
+    compindex.H_index = [5, 18:19];
+    compindex.R_index = [6, 20:21];
+    compindex.D_index = [7];
+    compindex.V_index = [22:23];
+
     data.compindex = compindex;
 
 
