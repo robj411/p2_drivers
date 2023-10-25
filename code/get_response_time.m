f6c9cbcb10fea8c8ec93d11865f7c353fde2601a
Rob Johnson <rob.johnson87@ntlworld.com>
2023-10-25 15:42:14 +0100

    add in periods

--- code/get_response_time.m
+++ code/get_response_time.m
@@ -9,28 +9,32 @@ function response_time = get_response_time(data, dis, hosp_trigger)
     R0 = dis.R0; 
     nsamples = 100;
     generationsam = zeros(1,nsamples);
+    hospsam = zeros(1,nsamples);
     for i = 1:nsamples
-        generationsam(i) = branchingProcess(R0, mean_ihr, hosp_trigger);
+        branch_out = branching_process(R0, mean_ihr, hosp_trigger);
+        generationsam(i) = branch_out(1);
+        hospsam(i) = branch_out(2);
     end
-    generations = mean(generationsam);
+    generations = mean(generationsam(hospsam>=hosp_trigger));
     
     generation_time = log(R0) / (log(2) / dis.Td);
-    response_time = generations * generation_time;
+    response_time = generations * generation_time + dis.Tlat + dis.Tsh;
 
 
 end
 
 
-function generations = branchingProcess(R0, mean_ihr, hosp_trigger)
+function branch_out = branching_process(R0, mean_ihr, hosp_trigger)
     % Initialize variables
     n_infections = [];
     hospitalisations = 0;
     generations = 1;
     n_infections(generations) = 5;
-    while hospitalisations < hosp_trigger & n_infections(generations) > 0
+    while hospitalisations < hosp_trigger && n_infections(generations) > 0
         generations = generations + 1;
         % In subsequent generations, each infected individual generates R0 offspring
         n_infections(generations) = poissrnd(R0 * n_infections(generations - 1));
         hospitalisations = hospitalisations + binornd(n_infections(generations), mean_ihr);
     end
+    branch_out = [generations, hospitalisations];
 end
