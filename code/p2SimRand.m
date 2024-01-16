773d09e70e41df15226647e5b9732e2d7d523b88
Robert Johnson <rj411@dide.local>
2024-01-16 12:26:24 +0000

    wip

--- code/p2SimRand.m
+++ code/p2SimRand.m
@@ -3,17 +3,17 @@
 
 income_levels = {'LLMIC','UMIC','HIC'};
 strategies = {'No Closures','School Closures','Economic Closures','Elimination'};
-vaccination_levels = {'BAU','100 days','BPSV'};
-countrytype_levels = {'Origin','Secondary'};
+vaccination_levels = [365, 100];
+bpsv_levels = [0, 1];
 
 nsamples  = 2048;
 n_income = numel(income_levels);
 
-synthetic_countries_base = cell(nsamples,length(income_levels));
+% synthetic_countries_base = cell(nsamples,length(income_levels));
 synthetic_countries = cell(nsamples,length(income_levels));
 synthetic_countries_dis = cell(nsamples,length(income_levels));
 synthetic_countries_dis_basis = cell(nsamples,1);
-synthetic_countries_p2 = cell(nsamples,length(income_levels),length(vaccination_levels));
+synthetic_countries_p2 = cell(nsamples,length(income_levels),length(vaccination_levels),length(bpsv_levels));
 
 %% country variables
 
@@ -56,103 +56,119 @@ for i = 1:nsamples
         
         % country data. random samples
         ldata1     = p2RandCountry(data,CD,income_level,country_parameter_distributions);
-        synthetic_countries_base{i,il} = ldata1;
+%         synthetic_countries_base{i,il} = ldata1;
+
+        dis1 = population_disease_parameters(ldata1,dis,R0_to_beta);
+
+        ldata1.rts = get_response_time(ldata1,dis1,ldata1.Hres);
         
         for vl = 1:length(vaccination_levels)
-            [ldata,dis2,p2] = p2Params(ldata1,dis,R0_to_beta,vl);
-            synthetic_countries_p2{i,il,vl} = p2;
+            for bl = 1:length(bpsv_levels)
+                [ldata,dis2,p2] = p2Params(ldata1,dis1,vaccination_levels(vl),bpsv_levels(bl));
+                synthetic_countries_p2{i,il,vl,bl} = p2;
+            end
         end
         synthetic_countries_dis{i,il} = dis2;
         synthetic_countries{i,il}     = ldata;
     end
 end
+clear synthetic_countries_dis_basis
 
 %% set up simulation
-columnnames = {'VLY','VSY',...
+inputcolumnnames = {'VLY','VSY',...
     'School_contacts','School_age','Working_age','Elders',...
     'Unemployment_rate','GDP','Labour_share','Work_contacts',...
-    'Hospitality_contacts','Hospital_capacity','Test_rate',...
-    'Hospital_response','Response_time',...
+    'Hospitality_contacts',...
+    'Hospital_capacity','Test_rate','Hospital_response','Response_time',...
     'Social_distancing_max','Social_distancing_rate','Self_isolation_compliance','Candidate_infectees',...
+    'Agriculture','Food_sector','International_tourism',...
+    'Remote_teaching_effectiveness','Importation_time',...
+    'Remote_quantile','Hospital_occupancy_at_response', 'Remaining_susceptible','Exit_wave',... 
+    'Doubling_time','Generation_time',...
     'R0','beta','Mean_IHR',...
     'Mean_HFR','Mean_IFR','Probability_symptomatic','Latent_period',...
     'Asymptomatic_period','Symptomatic_period','Time_to_hospitalisation','Time_to_discharge',...
-    'Time_to_death','Agriculture','Food_sector','International_tourism',...
-    'Remote_teaching_effectiveness','Importation_time','Hospital_occupancy_at_response',...
-    'Remote_quantile','Start_vaccination',... %,'Vaccine_uptake','Vaccination_rate'
-    'Cost','Deaths','School','GDP_loss'};
-inputs    = zeros(nsamples,length(columnnames)-4);
-outputs   = zeros(nsamples,4);
+    'Time_to_death'};
+outputcolumnnames = {'Cost','Deaths','School','GDP_loss'};
+columnnames = [inputcolumnnames outputcolumnnames ];
+inputs    = zeros(nsamples,length(inputcolumnnames));
+outputs   = zeros(nsamples,length(outputcolumnnames));
 
 %% simulate
 
 hts = zeros(nsamples,n_income);
-
+endsusctcell = cell(n_income,length(strategies));
 for il = 1:n_income
     income_level = income_levels{il};
     for ms = 1:length(strategies)
         strategy = strategies{ms};
+        ht = zeros(1,nsamples);
+        endsusct = zeros(length(vaccination_levels)*length(bpsv_levels),nsamples);
         for vl = 1:length(vaccination_levels)
-            for ct = 1:length(countrytype_levels)
-                ht = 730*ones(1,nsamples);
-                countrytype = countrytype_levels(ct);
+            for bl = 1:length(bpsv_levels)
+                endsusci = zeros(1,nsamples);
                 parfor i = 1:nsamples
                     dis2 = synthetic_countries_dis{i,il};
-                    p2 = synthetic_countries_p2{i,il,vl};
-                    ldata= synthetic_countries{i,il};
-
-                    [rdata, xoptim] = get_strategy_design(ldata,strategy,countrytype,p2);
-
-                    sec         = nan(1,4);
+                    ldata = synthetic_countries{i,il};                   
+                    p2 = synthetic_countries_p2{i,il,vl,bl};
+                    [rdata, xoptim] = get_strategy_design(ldata,strategy,p2);
                     int = 5;
                     try
-%                         disp([il ms vl ct i]);
-                        [~,f,g] = p2Run(rdata,dis2,strategy,int,xoptim,p2);
-%                         figure('Position', [100 100 400 300]); plot(f(:,1),f(:,7:10))
+
+                        [~,f,g,iseq] = p2Run(rdata,dis2,strategy,int,xoptim,p2);
+                        [~,exitwave] = min(abs(f(:,1)-iseq(end,1)));
+                        exitwavefrac = 1-sum(f(exitwave,7:10))/sum(f(end,7:10));
+        %                         figure('Position', [100 100 400 300]); plot(f(:,1),f(:,7:10))
+                        
+                        endsusc = f(end,11)/f(1,11);
+                        endsusci(i) = endsusc;  
                         ht(i) = f(find(f(:,1) > p2.Tres,1),3);
+
                         [cost,~]    = p2Cost(ldata,dis2,p2,g);
+                        sec         = nan(1,4);
                         sec(1)      = sum(cost([3,6,7:10],:),'all');
                         sec(2)      = sum(cost([3],:),'all');
                         sec(3)      = sum(cost([6],:),'all');
-                        sec(4)      = sum(cost([7:10],:),'all');
+                        sec(4)      = sum(cost([7:10],:),'all');        
+                        
+                        gdp = sum(ldata.obj);
+                        popsize = sum(ldata.Npop);
+                        working_age = popsize - sum(ldata.NNs([46,47,49]));
+                        unemployment_rate = ldata.NNs(48)/working_age;
+                        contacts = ldata.contacts;
+                        inputs(i,:)  = [ldata.vly ldata.vsy ...
+                            contacts.schoolA2 ldata.NNs(47)/popsize working_age/popsize ldata.NNs(49)/popsize...
+                            unemployment_rate ldata.gdp ldata.labsh contacts.workrel ...
+                            contacts.hospitality_frac...
+                            ldata.Hmax ldata.trate ldata.Hres p2.Tres...
+                            ldata.sdl ldata.sdb ldata.self_isolation_compliance dis2.CI ...
+                            ldata.obj([1 32])'/gdp ldata.frac_tourism_international ...
+                            ldata.remote_teaching_effectiveness ldata.t_import...
+                            ldata.remote_quantile ht(i) endsusc exitwavefrac...
+                            dis2.Td dis2.generation_time ...
+                            dis2.R0 dis2.beta mean(dis2.ihr) ...
+                            mean(dis2.hfr) mean(dis2.ifr) dis2.ps dis2.Tlat ...
+                            dis2.Tay dis2.Tsr dis2.Tsh dis2.Threc ...
+                            dis2.Thd ];
+                        outputs(i,:) = sec;
+
+                        if any(sec<0)
+                            disp(strcat(string(strategy),'_',string(income_level),'_',string(vaccination_levels(vl)),'_',string(bpsv_levels(bl)),'_',string(i),' 0'))
+                            disp(i)
+                        end
                     catch
-                        disp(strcat('VOI_',string(strategy),'_',string(income_level),'_',string(vaccination_levels(vl)),'.NA'))
-                        disp([il ms vl ct i]);
+                        disp(strcat(string(strategy),'_',string(income_level),'_',string(vaccination_levels(vl)),'_',string(bpsv_levels(bl)),'_',string(i),' NA'))
+                        disp([il ms vl bl i]);
                     end
-                    if any(sec<0)
-                        disp(strcat('VOI_',string(strategy),'_',string(income_level),'_',string(vaccination_levels(vl)),'.0'))
-                        disp(i)
-                    end
-
-                    outputs(i,:) = sec;
-
-                    gdp = sum(ldata.obj);
-                    popsize = sum(ldata.Npop);
-                    working_age = popsize - sum(ldata.NNs([46,47,49]));
-                    unemployment_rate = ldata.NNs(48)/working_age;
-                    contacts = ldata.contacts;
-                    inputs(i,:)  = [ldata.vly ldata.vsy ...
-                        contacts.schoolA2 ldata.NNs(47)/popsize working_age/popsize ldata.NNs(49)/popsize...
-                        unemployment_rate ldata.gdp ldata.labsh contacts.workrel ...
-                        contacts.hospitality_frac ldata.Hmax ldata.trate  ...
-                        ldata.Hres p2.Tres ldata.sdl ldata.sdb ldata.self_isolation_compliance dis2.CI ...
-                        dis2.R0 dis2.beta mean(dis2.ihr) ...
-                        mean(dis2.hfr) mean(dis2.ifr) dis2.ps dis2.Tlat ...
-                        dis2.Tay dis2.Tsr dis2.Tsh dis2.Threc ...
-                        dis2.Thd ldata.obj([1 32])'/gdp ldata.frac_tourism_international ...
-                        ldata.remote_teaching_effectiveness ldata.t_import ht(i)...
-                        ldata.remote_quantile ldata.t_vax]; % ldata.arate ldata.puptake
-                end
-                
-                if vl==1 & ms==1 & ct==1
-                    hts(:,il) = ht;
-                end
-                T                          = array2table([inputs,outputs]);
+                end   
+                endsusct((vl-1)*length(bpsv_levels)+bl,:) = endsusci;
+                T = array2table([inputs outputs]);
                 T.Properties.VariableNames = columnnames;
-
-                writetable(T,strcat('results/VOI_',string(strategy),'_',string(income_level),'_',string(vaccination_levels(vl)),'_',string(countrytype),'.csv'));
+                writetable(T,strcat('results/VOI_',string(strategy),'_',string(income_level),'_',string(vaccination_levels(vl)),'_',string(bpsv_levels(bl)),'.csv'));
             end
         end
+        printendsusct = [(1:nsamples)/1000; endsusct];
+        disp(printendsusct(:,printendsusct(5,:)+1e-2<printendsusct(3,:)|printendsusct(4,:)+1e-2<printendsusct(2,:)));
     end
 end
 
