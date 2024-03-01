152659cf3678d3e61e21ce6ccbad99cf855252ca
Rob Johnson <rob.johnson87@ntlworld.com>
2024-03-01 09:44:07 +0000

    comments

--- code/functions/social_distancing.m
+++ code/functions/social_distancing.m
@@ -11,23 +11,18 @@
 
 % function sd_out = social_distancing(lower_bound,rate,deaths_per_10k,current_drop ) 
 function sd_out = social_distancing(baseline, death_coef, mandate_coef,...
-                                    deaths_per_100k,rel_mobility, rel_stringency) 
-
-%     rel_mobility = 1-current_drop ;
+                                    deaths_per_mil,rel_mobility, rel_stringency) 
     
+    % same function as in R code where parameters fitted
+    sd = 1./(1+death_coef.*deaths_per_mil + mandate_coef.*rel_stringency) .* (1-baseline) + baseline;
     
-%     sd = (lower_bound-rate)+(1-lower_bound+rate)*(1+((lower_bound-1)/(1-lower_bound+rate))).^(deaths_per_10k./10);
-    sd = 1./(1+death_coef.*deaths_per_100k + mandate_coef.*rel_stringency) .* (1-baseline) + baseline;
-    sd = min(rel_mobility, max(sd, baseline)); % if sd computed is higher than mandate, we should return 1
+    % if sd computed is higher than mandate, we should return 1
+    sd = min(rel_mobility, max(sd, baseline)); 
     
+    % divide through as we already account for the mandate impact on
+    % transmission
     sd_out = sd./rel_mobility;
     
-%     max_final_reduction = 1 - baseline;
-%     maxdrop = min(maxdrop, max_final_reduction-current_drop);
-%     
-%     final_reduction = current_drop + (1-sd).*maxdrop;
-%     final_mobility = 1-final_reduction;
-%     sd_out = final_mobility./rel_mobility;
 
 end
 %        sd_fun = @(l,b,x) (l-b)+(1-l+b)*(1+((l-1)/(1-l+b))).^(x./10);
