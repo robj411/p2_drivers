f3d800a407bc09f2a09b72521547006d03fb87aa
Rob Johnson <rob.johnson87@ntlworld.com>
2024-03-04 12:00:36 +0000

    definitions for infectiousness averted by self isolating. isolate to end of simulation

--- code/functions/fraction_averted_self_isolating.m
+++ code/functions/fraction_averted_self_isolating.m
@@ -15,16 +15,27 @@ function [p3, p4] = fraction_averted_self_isolating(sumI, sumN, p2, t, mandate)
 
     test_start_time = p2.t_tit;
     test_end_time = max(p2.tpoints);
-    valid_timepoints = (t>=test_start_time).*(t<=test_end_time);
+    valid_timepoints = (t>=test_start_time);%.*(t<=test_end_time);
         
-    if any(valid_timepoints==1) & mandate~=5
+    if any(valid_timepoints==1) %& mandate~=5
         Ip = 10^5*sumI/sumN;
-        p3 = get_case_ID_rate(p2, Ip);
+        frac_cases_found = get_case_ID_rate(p2, Ip);
+        
+        p3 = frac_cases_found * p2.frac_asym_infectiousness_averted ;
         p3 = p3.*(valid_timepoints); 
-        p4 = 1 - p2.frac_sym_infectiousness_remaining;
+        
+        presym_averted = frac_cases_found * p2.frac_presym_infectiousness_averted ;
+        presym_averted = presym_averted.*(valid_timepoints); 
+        sym_averted = p2.frac_sym_infectiousness_averted + presym_averted;
+        p4 = sym_averted;
     else
         p3 = zeros(size(t));
-        p4 = p3;
+        if t>=test_start_time % instruction to self isolate if unwell
+            p4 = p2.frac_sym_infectiousness_averted;
+%             p4 = p3;
+        else
+            p4 = p3;
+        end
     end
     
 %     p4 = p3;
