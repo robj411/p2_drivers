9b67cef7c53b982b9ae2b2add9751546e2a592bb
Robert Johnson <rj411@dide.local>
2024-03-09 18:49:05 +0000

    normalise sd using median

--- code/functions/get_R.m
+++ code/functions/get_R.m
@@ -22,7 +22,7 @@ function R = get_R(nStrata, dis, S,Sv1,Sv2, beta, p3, p4, ddk, data, mandate)
     
     foi0 = data.basic_foi;
     foi1 = contact_matrix*(1./N);
-    sd_so_far = (foi1+1e-10)./(foi0+1e-10);
+    sd_so_far = median((foi1+1e-10)./(foi0+1e-10));
 
     sd = betamod_wrapped(ddk, data, mandate);
     betamod = sd./sd_so_far;
