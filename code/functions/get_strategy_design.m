c7297ec0f8d826823618d91e9d17b2ea78df56d9
Robert Johnson <rj411@dide.local>
2024-04-26 11:59:40 +0100

    harmonise strategies

--- code/functions/get_strategy_design.m
+++ code/functions/get_strategy_design.m
@@ -18,26 +18,31 @@ function [data, configuration] = get_strategy_design(data,strategy,p2)
     nSectors = data.nSectors;
     
     configuration = 0;
+    x_unmit = data.x_unmit;
+    x_elim = data.x_elim;
+    x_schc = data.x_schc;
+    x_econ = data.x_econ;
+    wfh = data.wfh;
     if strcmp(strategy,'Elimination')
-        configuration      = [ones(1*nSectors,1);data.x_econ(:,2);data.x_elim(:,1);ones(2*nSectors,1)];
-        data.hw    = [zeros(1,nSectors);data.wfh(2,:);data.wfh(1,:);zeros(2,nSectors)];
+        configuration      = [ones(1*nSectors,1);x_econ(:,2);x_elim(:,1);ones(2*nSectors,1)];
+        data.hw    = [zeros(1,nSectors);wfh(2,:);wfh(1,:);zeros(2,nSectors)];
         data.imand = [2];
         data.inext = [2,2,3,2,5];
     elseif strcmp(strategy,'Economic Closures')
-        configuration      = [ones(1*nSectors,1);data.x_unmit;data.x_econ(:,2);data.x_econ(:,1);ones(nSectors,1)];
-        data.hw    = [zeros(1,nSectors);data.wfh(1,:);data.wfh(2,:);data.wfh(1,:);zeros(1,nSectors)];
+        configuration      = [ones(1*nSectors,1);x_unmit;x_econ(:,2);x_econ(:,1);ones(nSectors,1)];
+        data.hw    = [zeros(1,nSectors);wfh(1,:);wfh(2,:);wfh(1,:);zeros(1,nSectors)];
         data.imand = [3];
         data.inext = [2,3,3,4,5];
     elseif strcmp(strategy,'School Closures')
-        configuration      = [ones(1*nSectors,1);data.x_unmit;data.x_schc(:,2);data.x_schc(:,1);ones(nSectors,1)];
-        data.hw    = [zeros(1,nSectors);data.wfh(1,:);data.wfh(2,:);data.wfh(1,:);zeros(1,nSectors)];
+        configuration      = [ones(1*nSectors,1);x_unmit;x_schc(:,2);x_schc(:,1);ones(nSectors,1)];
+        data.hw    = [zeros(1,nSectors);wfh(1,:);wfh(2,:);wfh(1,:);zeros(1,nSectors)];
         data.imand = [3];
         data.inext = [2,3,3,4,5];
     elseif strcmp(strategy,'No Closures')
-        configuration      = [ones(1*nSectors,1); repmat(data.x_unmit,3,1); ones(1*nSectors,1)];
+        configuration      = [ones(1*nSectors,1); repmat(x_unmit,3,1); ones(1*nSectors,1)];
         data.hw    = [zeros(5,nSectors)];
         data.imand = [10];
-        data.inext = [2,2,5];
+        data.inext = [2,2,2,2,5];
     end
     
     data.tvec = [-0.1 data.tvec(end)];
