d829d7721ad93a022770045200f894f4a0b8770a
rj <25404855+robj411@users.noreply.github.com>
2026-05-26 16:47:27 +0100

    add overall script

---
+++ code/run_all_scripts.R
@@ -0,0 +1,23 @@
+
+if(!file.exists('process_results.R')){
+  setwd(getSrcDirectory(function(){})[1])
+}
+
+# get costs and deliveries
+
+setwd('costing')
+source('costing_script.R')
+setwd('..')
+
+# run matlab impact model
+require(matlabr)
+matlabr::run_matlab_script('impact_script.m')
+
+# aggregate impacts to pandemics
+source('aggregation_script.R')
+
+# build results markdown
+require(rmarkdown)
+setwd('..')
+rmarkdown::render('outputs.Rmd', 'bookdown::pdf_document2', params=list(lbfile ='data/vaccine_delivery.xlsx'), clean=F)
+
